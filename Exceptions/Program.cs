8df064cabefca970ac40a3f5ccf9006c15e1e8ee
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-11-23 21:51:51 +0100

    first commit

---
+++ Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* try
+             {
+                 using (StreamReader sr = File.OpenText("data.txt"))
+                 {
+                     Console.WriteLine($"The first line of this file is {sr.ReadLine()}");
+                 }
+             }
+             catch (FileNotFoundException e)
+             {
+                 Console.WriteLine($"The file was not found: '{e.Message}'");
+             }
+             catch (DirectoryNotFoundException e)
+             {
+                 Console.WriteLine($"The directory was not found: '{e.Message}'");
+             }
+             catch (IOException e)
+             {
+                 Console.WriteLine($"The file could not be opened: '{e.Message}'");
+             }catch (Exception e)
+             {
+                 Console.WriteLine(e.Message);
+             }*/
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Exceptions/UserNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class UserNotFoundException : Exception
+{
+	public UserNotFoundException()
+	{
+        public string UserName { get; }
+
+    public override string Message => "User Not Found!";
+
+    public UserNotFoundException() { }
+
+        public UserNotFoundException(string message)
+        : base(message) { }
+
+        public UserNotFoundException(string message, Exception inner)
+        : base(message, inner) { }
+
+    public UserNotFoundException(string userName)
+    {
+        UserName = userName;
+    }
+}
+}
